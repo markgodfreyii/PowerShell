8193528f7ee31fe7cbe1bd29dc0c47672f50ac1b
markgodfreyii <markgodfreyii@hotmail.com>
2017-08-25 10:02:28 -0500

    Add files via upload

---
+++ Create-LocalAdmin.ps1
@@ -0,0 +1,15 @@
+﻿<#
+Create new local admin account with randomly generated password
+Created by Mark Godfrey @Geodesicz
+Requires PowerShell 5.1 or newer with Microsoft.PowerShell.LocalAccounts module
+#>
+
+[CmdletBinding()]
+Param(
+    [Parameter(HelpMessage="Username")]
+    [ValidateNotNullOrEmpty()]
+    [string]$Username
+)
+
+$User = New-LocalUser -AccountNeverExpires -Name $Username -Password (ConvertTo-SecureString (-join ((33..126) | Get-Random -Count 16 | foreach {[char]$PSItem})) -AsPlainText -Force)
+Add-LocalGroupMember -Group "Administrators" -Member $User
